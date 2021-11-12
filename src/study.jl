9f17dde27fa6274e3b8578a01465dbc170dbab9a
archanarw <47085068+archanarw@users.noreply.github.com>
2021-11-13 03:27:05 +0530

    updated study

--- src/study.jl
+++ src/study.jl
@@ -20,5 +20,115 @@ end
 
 # study_3
 function study_3_sat(li::Array{Int64,1})
-  issorted(li) == collect(0:999) & all(li[i] != i for i in 0:999)
+  (issorted(li) == collect(0:999)) & (all(li[i] != i for i in 0:999))
+end
+
+# study 4
+"Find a list of length 10 where the fourth element occurs exactly twice."
+function study_4_sat(li::Array{Int64, 1})
+  (length(li) == 10) & (count(x -> x == li[4], li) == 2)
+end
+
+# study 5
+"Find a list integers such that the integer i occurs i times, for i = 0, 1, 2, ..., 9."
+function study_5_sat(li::Array{Int64, 1})
+  all([count(x -> x == i, li) == i for i in 0:9])
+end
+
+# study 6
+"Find an integer greater than 10^10 which is 4 mod 123."
+function study_6_sat(i::Int64)
+  (i % 123 == 4) & (i > 10 ^ 10)
+end
+
+# study 7
+"Find a three-digit pattern  that occurs more than 8 times in the decimal representation of 8^2888."
+function study_7_sat(s::String)
+    (count(s, string(8 ^ BigInt(2888))) > 8) & (length(s) == 3)
+end
+
+# study 8
+"Find a list of more than 1235 strings such that the 1234th string is a proper substring of the 1235th."
+function study_8_sat(ls::Array{String, 1})
+    (occursin(ls[1234], ls[1235])) & (ls[1234] != ls[1235])
+end
+
+# study 9
+"""
+ Find a way to rearrange the letters in the pangram "The quick brown fox jumps over the lazy dog" to get
+ the pangram "The five boxing wizards jump quickly". The answer should be represented as a list of index
+ mappings.
+"""
+function study_9_sat(li::Array{Int64, 1})
+    ["The quick brown fox jumps over the lazy dog"[i] for i in li] ==
+            [i for i in "The five boxing wizards jump quickly"]
+end
+
+# study 10
+"Find a palindrome of length greater than 11 in the decimal representation of 8^1818."
+function study_10_sat(s::String)
+    (s in string(8 ^ BigInt(1818))) & (s == s[1 : end - 1]) & (length(s) > 11)
+end
+
+# study 11
+"""
+ Find a list of strings whose length (viewed as a string) is equal to the lexicographically largest element
+ and is equal to the lexicographically smallest element.
+"""
+# function study_11_sat(ls::Array{String, 1})
+#     min(split(ls, "")) == max(split(ls, "")) == string(length(ls))
+# end
+
+# study 12
+"""
+ Find a list of 1,000 integers where every two adjacent integers sum to 9, 
+ and where the first integer plus 4 is 9.
+"""
+function study_12_sat(li::Array{Int64, 1})
+    all(li[i] + li[i+1] == 9 for i in 1:length(li)) & (li[1] == 5) & (length(li) == 1000)
+end
+
+# study 13
+"Find a real number which, when you subtract 3.1415, has a decimal representation starting with 123.456."
+function study_13_sat(x::Float64)
+    string(x - 3.1415)[1:7] == "123.456"
+end
+
+# study 14
+"Find a list of integers such that the sum of the first i integers is i, for i=0, 1, 2, ..., 19."
+function study_14_sat(li::Array{Int64, 1})
+    all([sum(li[1:i]) == i for i in 0:19])
+end
+
+# study 15
+"Find a list of integers such that the sum of the first i integers is 2^i -1, for i = 0, 1, 2, ..., 19."
+function study_15_sat(li::Array{Int64, 1})
+    all(sum(li[1:i]) == 2 ^ i - 1 for i in 0:19)
+end
+
+# study 16
+"""
+ Find a real number such that when you add the length of its decimal representation to it, you get 4.5.
+ Your answer should be the string form of the number in its decimal representation.
+"""
+function study_16_sat(s::String)
+    parse(Float64, s) + length(s) == 4.5
+end
+
+# study 17
+"""
+ Find a number whose decimal representation is *a longer string* when you add 1,000 to it than 
+ when you add 1,001.
+"""
+function study_17_sat(i::Int64)
+    length(string(i + 1000)) > length(string(i + 1001))
+end
+
+# study 18
+"""
+ Find a list of strings that when you combine them in all pairwise combinations gives the six strings:
+ 'berlin', 'berger', 'linber', 'linger', 'gerber', 'gerlin'
+"""
+function study_18_sat(ls::Array{String, 1})
+    [s + t for s in ls for t in ls if s != t] == ["berlin", "berger", "linber", "linger", "gerber", "gerlin"]
 end
--- src/study_sol.jl
+++ src/study_sol.jl
@@ -4,4 +4,61 @@ solves(::typeof(study_1_sat)) = study_1_sol
 study_2_sol() = "ho" ^ (800 + 1) * "o" ^ (100 * 2 - 1)
 solves(::typeof(study_2_sat)) = study_1_sol
 
-# study_3_sol() = [((i + 1) % 999) for i in 0:999]
+study_3_sol() = [((i + 1) % 999) for i in 0:999]
+solves(::typeof(study_3_sat)) = study_3_sol
+
+study_4_sol() = repeat(collect(0:4), 2)
+solves(::typeof(study_4_sat)) = study_4_sol
+
+study_5_sol() = reduce(vcat, Int64[i*ones(Int64, i) for i in 1:9]...)
+solves(::typeof(study_5_sat)) = study_5_sol
+
+study_6_sol() = 4 + 10 ^ 10 + 123 - 10 ^ 10 % 123
+solves(::typeof(study_6_sat)) = study_6_sol
+
+function study_7_sol()
+    s = string(8^BigInt(2888))
+    t = unique([s[i : i + 2] for i in 1:(length(s) - 2)])
+    max(count.(t, s)...)
+end
+solves(::typeof(study_7_sat)) = study_7_sol
+
+study_8_sol() = vcat(repeat([""], 1234),  ["a"])
+solves(::typeof(study_8_sat)) = study_8_sol
+
+study_9_sol() = Int64[findfirst(t, "The quick brown fox jumps over the lazy dog")
+    for t in "The five boxing wizards jump quickly"]
+solves(::typeof(study_9_sat)) = study_9_sol
+
+# function study_10_sol()
+#     s = 8 ^ BigInt(1818)
+#     for l in 12:length(s)
+#         for i in 1:(length(s) - l)
+#             if s[i: i + l] == reverse(s[i: i + l])
+#                 return s[i: i + l]
+#             end
+#         end
+#     end
+# end
+# solves(::typeof(study_10_sat)) = study_10_sol
+
+study_11_sol() = ["1"]
+# solves(::typeof(study_11_sat)) = study_11_sol
+
+study_12_sol() = reduce(vcat, repeat([9 - 4, 4], 500))
+solves(::typeof(study_12_sat)) = study_12_sol
+
+study_13_sol() = 123.456 + 3.1415
+solves(::typeof(study_13_sat)) = study_13_sol
+
+study_14_sol() = ones(Int64, 20)
+solves(::typeof(study_14_sat)) = study_14_sol
+
+study_15_sol() = Int64[(2 ^ i) for i in 0:19]
+solves(::typeof(study_15_sat)) = study_15_sol
+
+study_16_sol() = string(4.5 - length(string(4.5)))
+solves(::typeof(study_16_sat)) = study_16_sol
+
+study_17_sol() = -1001
+solves(::typeof(study_17_sat)) = study_17_sol
