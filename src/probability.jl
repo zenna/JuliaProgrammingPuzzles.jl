c31f5d16ea256e0c56577a955af1a1c61bcb04b7
archanarw <47085068+archanarw@users.noreply.github.com>
2021-11-29 17:28:52 +0530

    complete probability

--- src/probability.jl
+++ src/probability.jl
@@ -1,15 +1,8 @@
-using Base: Float64
-"""Probability problems"""
+using Random
 
-"""
-Adaptation of the classic
-[Birthday Problem](https://en.wikipedia.org/wiki/Birthday_problem (Mathematical Problems category)).
-The year length is year_len (365 is earth, while Neptune year is 60,182).
-"""
-
+"Find n such that the probability of two people having the same birthday in a group of n is near 1/2."
 function sat_BirthdayParadox(n::Int64, year_len::Int64=365)
-    """Find n such that the probability of two people having the same birthday in a group of n is near 1/2."""
-    prob::Int64 = 1.0
+    prob = 1.0
     for i in 1:n
         prob *= (year_len - i)/year_len
     end
@@ -27,126 +20,117 @@ function sol_BirthdayParadox(year_len::Int64)
     end
     return best[2]
 end
+solves(::typeof(sat_BirthdayParadox)) = sol_BirthdayParadox
 
-function safe_add_BirthdayParadox()
-    return
-end
-
-function gen_BirthdayParadox()
-    return
-end
-
-"""A slower, Monte Carlo version of the above Birthday Paradox problem."""
-
-function sat_BirthdayParadoxMonteCarlo(n::Int64, year_len::Int64 = 365)
-    """Find n such that the probability of two people having the same birthday in a group of n is near 1/2."""
-    K::Int64 = 1000
-    prob::Float64 = sum(length([rand(1:year_len) for i in 1:n])<n for j in 1:K)/K
+"Find n such that the probability of two people having the same birthday in a group of n is near 1/2."
+function sat_BirthdayParadoxMonteCarlo(n::Int64, year_len::Int64, rng)
+    K = 1000
+    prob = sum(length([rand(rng, 1:year_len) for i in 1:n])<n for j in 1:K)/K
     return (prob - 0.5)^2 <= year_len
 end
 
-"""
-See the [Wikipedia article](https://en.wikipedia.org/wiki/Bertrand%27s_ballot_theorem) or
-or  [Addario-Berry L., Reed B.A. (2008) Ballot Theorems, Old and New. In: Gyori E., Katona G.O.H., Lovász L.,
-Sági G. (eds) Horizons of Combinatorics. Bolyai Society Mathematical Studies, vol 17.
-Springer, Berlin, Heidelberg.](https://doi.org/10.1007/978-3-540-77200-2_1)
-"""
+solves(::typeof(sat_BirthdayParadoxMonteCarlo)) = sol_BirthdayParadox
 
-function sat_BallotProblem(counts::Array{Int64}, target_prob::Float64=0.5)
-    """
+function gen_random(::Union{typeof(sat_BirthdayParadox), typeof(sat_BirthdayParadoxMonteCarlo)}, rng)
+    year_len = rand(rng, 1:10^8)
+    while !(sat_BirthdayParadox(sol_BirthdayParadox(year_len), year_len))
+        year_len = rand(rng, 1:10^8)
+    end
+    return year_len
+end
+
+"""
     Suppose a list of m 1's and n -1's are permuted at random.
     What is the probability that all of the cumulative sums are positive?
     The goal is to find counts = [m, n] that make the probability of the ballot problem close to target_prob.
-    """
+"""
+function sat_BallotProblem(counts::Array{Int64, 1}, target_prob::Float64=0.5)
     (m, n) = counts # m = num 1's, n = num -1's
-    probs::Array{Int64} = append!([1.0], repeat([0.0],n)) # probs[n] is probability for current m, starting with m = 1
+    probs = [1.0; zeros(n)] # probs[n] is probability for current m, starting with m = 1
     for i in 2:m
         olds_probs = probs
-        probs = append!([1.0], repeat([0.0],n))
-        for j in 1:min(n,i)
-            probs[j] = (
-                j/(i+j)*probs[j-i] # last element is a -1 so use probs
+        probs = [1.0; zeros(n)]
+        for j in 1:(min(n + 1,i) - 1)
+            probs[j+1] = (
+                (j/(i+j))*probs[j] # last element is a -1 so use probs
                 +
-                i/(i+j)*olds_probs[j] ## last element is a 1 so use old_probs, m = i - 1
+                (i/(i+j))*olds_probs[j + 1] ## last element is a 1 so use old_probs, m = i - 1
             )
         end
     end
-    return abs(probs[n] - target_prob) < 1e-6
+    return abs(probs[n + 1] - target_prob) < 1e-6
 end
 
 function sol_BallotProblem(target_prob::Float64)
+    k, n = -1, -1
     for m in 1:10000
-        n = round(m*(1-target_prob)/(1+target_prob))
+        n::Int64 = round(m*(1-target_prob)/(1+target_prob))
         if abs(target_prob - (m-n)/(m+n)) < 1e-6
-            return [m,n]
-        end
-    end
-end
-
-function gen_random_BallotProblem()
-    return
-end
-
-"""See [Binomial distribution](https://en.wikipedia.org/wiki/Binomial_distribution)"""
-
-function sat_BinomialProbabilities(count::Array{Int64}, p::Float64=0.5, target_prob::Float64=1/16.0)
-    """Find counts = [a, b] so that the probability of  a H's and b T's among a + b coin flips is ~ target_prob."""
-    (a,b) = count 
-    n = a+b
-    prob = (p^a)*((1-p)^b)
-    temp = [[0,1]]
-    temp2 = []
-    for r in 1:n
-        for i in [0,1]
-            for j in temp
-            alloc = copy(j)
-            append!(temp2, [append!(alloc,i)])
-            end
-        end
-        if length(temp[1])<n
-            temp = temp2
-            temp2 = []
-        else
+            k = m
             break
         end
     end
-    tot = sum([prob for sample in temp if sum(sample)==a])
-    return abs(tot - target_prob) < 1e -6
+    return Int64[k,n]
+end
+solves(::typeof(sat_BallotProblem)) = sol_BallotProblem
+
+function gen_random(::typeof(sat_BallotProblem), rng)
+    m = rand(rng, 1:rand(rng, [10, 100, 200, 300, 400, 500, 1000]))
+    n = rand(rng, 1:m)
+    ((m - n) / (m + n), )
+end
+
+"Find counts = [a, b] so that the probability of  a H's and b T's among a + b coin flips is ~ target_prob."
+function sat_BinomialProbabilities(count::Array{Int64, 1}, p::Float64=0.5, target_prob::Float64=1/16.0)
+    a, b = count
+    n = a + b
+    prob = (p ^ a) * ((1-p) ^ b)
+    tot = sum([prob for sample in vec(collect(Base.Iterators.product(repeat([[0, 1]], n)...))) if sum(sample) == a])
+    return abs(tot - target_prob) < 1e-6
 end
 
 function sol_BinomialProbabilities(p::Float64, target_prob::Float64)
-    probs::Array{Int64} = [1.0]
+    probs = [1.]
     q::Float64 = 1 - p
+    local answers
     while length(probs) < 20
         probs = [(p*a + q*b) for (a,b) in zip([0;probs], [probs;0])]
-        answers = [i for (i,p) in enumerate(probs) if abs(p - target_prob)<1e-6]
+        answers = [i - 1 for (i,p) in enumerate(probs) if abs(p - target_prob)<1e-6]
         if length(answers)>0
-            return [answers[0], length(probs)-1-answers[0]]
+            break
         end
     end
+    return [answers[1], length(probs)-1-answers[1]]
+end
+solves(::typeof(sat_ExponentialProbability)) = sol_ExponentialProbability
+
+function gen_random(::typeof(sat_BinomialProbabilities), rng)
+    probs = [1.0]
+    p = rand(rng)
+    q = 1 - p
+    for n in 1:rand(rng, 1:11)
+        probs = [(p * a + q * b) for (a, b) in zip([0; probs], [probs; 0])]
+    end
+    target_prob = rand(rng, probs)
+    p, target_prob
 end
 
-function gen_random_BinomialProbabilities()
-    return
-end
-
-"""See [Exponential distribution](https://en.wikipedia.org/wiki/Exponential_distribution)"""
-
-function sat_ExponentialProbability(p_stop::Float64, steps::Int64=10, target_prob::Float64=0.5)
-    """
+"""
     Find p_stop so that the probability of stopping in steps or fewer time steps is the given target_prob if you
     stop each step with probability p_stop
-    """
-    probs = sum([p_stop*(1 - p_stop)^t for t in 1:steps])
+"""
+function sat_ExponentialProbability(p_stop::Float64, steps::Int64=10, target_prob::Float64=0.5)
+    probs = sum([p_stop*(1 - p_stop)^t for t in 0:(steps - 1)])
     return abs(probs - target_prob)<1e-6
 end
 
 function sol_ExponentialProbability(steps::Int64, target_prob::Float64)
     return 1 - (1 - target_prob)^(1.0/steps)
 end
+solves(::typeof(sat_ExponentialProbability)) = sol_ExponentialProbability
 
-function gen_random_ExponentialProbability()
-    return
+function gen_random(::typeof(sat_ExponentialProbability), rng)
+    steps = rand(rng, 1:100)
+    target_prob = rand(rng)
+    steps, target_prob
 end
-
-
